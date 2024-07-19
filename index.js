fefe9488524a9b999ae0a61e08795ca38c171697
Prabhat Saini <Prabhat.saini@binmile.com>
2024-07-19 10:31:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const callbackasync=()=>{
+    setTimeout(()=>{
+        console.log(typeof`${23}`)
+    },10000)
+};
+
+
+
+const callbacksync=(a)=>{
+    console.log(`number   a ${a}`);
+
+
+}
+
+
+const fun=(callbackasync)=>{
+callbackasync();
+}
+
+const fun1=(callbacksync)=>{
+callbacksync(34);
+}
+fun1(callbacksync);
+fun(callbackasync);
+console.log("hiiiiiiiiiiiiiiiiii");
